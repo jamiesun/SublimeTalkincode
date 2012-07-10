360980e2547a55c57390ed15c001a799c49dcf61
jamiesun <jamiesun.net@gmail.com>
2012-07-10 20:48:31 +0800

    ...

--- SublimeTalkincode.py
+++ SublimeTalkincode.py
@@ -8,11 +8,19 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 """
-@description:talkincode.org  sublime text plugin 
+@description:talkincode.org  sublime text 2 plugin 
 @tags:python,sublime text 2
+@author:jamiesun.net@gmail.com
 """
-logger = api.logger 
 settings = sublime.load_settings("SublimeTalkincode.sublime-settings")
+
+register_flag = "Talkincode.org Register:"
+codeid_flag = "Talkincode.org @codeid:"
+postid_flag = "Talkincode.org @postid:"
+
+########################################################################################
+####   Talkincode.org Register                  
+########################################################################################
 
 class RegisterTicForm(sublime_plugin.TextCommand):
     def __init__(self,view):
@@ -21,25 +29,25 @@ class RegisterTicForm(sublime_plugin.TextCommand):
     def run(self, edit):
         rview = sublime.active_window().new_file()
         sublime.active_window().focus_view(rview)
-        rview.insert(edit,0,"Talkincode Register:\n@username:\n@password:\n@email:")
+        rview.insert(edit,0,"%s\n@username:\n@password:\n@email:\n"%register_flag)
         self._visible = False
 
     def is_visible(self):
-        return self.view.find(r"Talkincode Register:",0,sublime.IGNORECASE) is None
+        return self.view.find(register_flag,0,sublime.IGNORECASE) is None
 
 
 class RegisterTic(sublime_plugin.TextCommand):
     def __init__(self,view):
         self.view = view
     def is_visible(self):
-        return self.view.find("Talkincode Register:",0) is not None      
+        return self.view.find(register_flag,0) is not None      
 
     def run(self, edit):
         region = sublime.Region(0L, self.view.size())
         content = self.view.substr(region)
         username_grp = re.search("@username:(.*)\n",content)
         passwd_grp = re.search("@password:(.*)\n",content)
-        email_grp = re.search("@email:(.*)",content)
+        email_grp = re.search("@email:(.*)\n",content)
 
         username = username_grp and username_grp.group(1)
         passwd = passwd_grp and passwd_grp.group(1)
@@ -70,16 +78,154 @@ class RegisterTic(sublime_plugin.TextCommand):
             except Exception, e:
                 sublime.error_message("register user error  error %s "%e)
             
-
+########################################################################################
+####   Talkincode.org search                
+########################################################################################
 
 class QueryTicCodes(sublime_plugin.WindowCommand):
     def run(self):      
-        pass
+        sublime.status_message("search code, please wait......")
+        def on_input(keywd):
+            try:
+                result = api.list_codes(keywd)
+                if type(result) ==dict and result.has_key("error"):
+                    raise Exception(result.get("error"))
+
+                def format_it(row):
+                    return  ["%s - %s"%(row["lang"],row["title"]),
+                             "by @%s <%s> hits : %s"%(row["author"],row["email"],row["hits"] )]
+                items = [format_it(row) for row in result]
+
+                def on_code_click(idx):
+                    if idx == -1:
+                        return
+                    uid = result[idx]["id"]
+                    lang = api.get_lang_ext(result[idx]["lang"])
+                    code_file_path = "%s/%s.%s"%(os.environ["TMP"],uid,lang)
+
+                    codeobj = api.get_code(uid)
+                    if type(codeobj) ==dict and codeobj.has_key("error"):
+                        raise Exception(codeobj.get("error"))
+
+                    code_file = open(code_file_path,"wb")
+                    code_file.write("%s%s\n"%(codeid_flag,uid))
+                    code_file.write(codeobj['content'])
+                    code_file.close()
+
+                    code_view = self.window.open_file(code_file_path)
+                    self.window.focus_view(code_view)
+
+                self.window.show_quick_panel(items,on_code_click)            
+            except Exception, e:
+                sublime.error_message("error:%s"%e)            
+
+        self.window.show_input_panel("search keyword::","",on_input,None,None)
 
         
 class QueryTicPosts(sublime_plugin.WindowCommand):
     def run(self):      
-        pass
+        sublime.status_message("search topic, please wait......")
+        def on_input(keywd):
+            try:
+                result = api.list_posts(keywd)
+                if not result:
+                    return
+
+                if type(result) ==dict and result.has_key("error"):
+                    raise Exception(result.get("error"))
+
+                def format_it(row):
+                    return  ["%s - %s"%(row["title"],row["tags"]),
+                             "by @%s  hits : %s %s"%(row["username"],row["hits"],row["created"]  )]
+                items = [format_it(row) for row in result]
+
+                def on_post_click(idx):
+                    if idx == -1:
+                        return
+                    uid = result[idx]["id"]
+                    post_file_path = "%s/%s.md"%(os.environ["TMP"],uid)
+
+                    post_result = api.get_post(uid)
+                    if not post_result:
+                        raise Exception(post_result.get("content not exists"))
+                    if type(post_result) ==dict and post_result.has_key("error"):
+                        raise Exception(post_result.get("error"))
+
+                    postobj = post_result['post']
+                    comments = post_result['comments']
+                    code_file = open(post_file_path,"wb")
+                    code_file.write("%s%s\n\n"%(postid_flag,uid))
+                    code_file.write("## title:%s\n"%postobj['title'])
+                    code_file.write("="*80)
+                    code_file.write("\n\n")
+                    code_file.write(postobj['content'])
+                    code_file.write("\n")
+                    if comments:
+                        for cm in comments:
+                            code_file.write("\n")
+                            code_file.write("* "*40)
+                            code_file.write("\n\n")
+                            code_file.write(cm['content'])
+                            code_file.write("\n\n")
+                            code_file.write("%s %s\n"%(cm['author'],cm["created"]))
+                    code_file.close()
+
+                    code_view = self.window.open_file(post_file_path)
+                    self.window.focus_view(code_view)
+
+                self.window.show_quick_panel(items,on_post_click)            
+            except Exception, e:
+                sublime.error_message("error:%s"%e)            
+
+        self.window.show_input_panel("search keyword::","",on_input,None,None)
+
+class RefreshTicPost(sublime_plugin.TextCommand):
+    def __init__(self,view):
+        self.view = view
+    def is_visible(self):
+        return self.view.find(postid_flag,0) is not None      
+
+    def run(self, edit):    
+        region = sublime.Region(0L, self.view.size())
+        content = self.view.substr(region)
+        postid_grp = re.search("%s(.*)\n"%postid_flag,content)
+        uid = postid_grp and postid_grp.group(1)
+        try:
+            post_file_path = "%s/%s.md"%(os.environ["TMP"],uid)
+            post_result = api.get_post(uid)
+            if not post_result:
+                raise Exception(post_result.get("content not exists"))
+            if type(post_result) ==dict and post_result.has_key("error"):
+                raise Exception(post_result.get("error"))
+
+            postobj = post_result['post']
+            comments = post_result['comments']
+            code_file = open(post_file_path,"wb")
+            code_file.write("%s%s\n\n"%(postid_flag,uid))
+            code_file.write("## title:%s\n"%postobj['title'])
+            code_file.write("="*80)
+            code_file.write("\n\n")
+            code_file.write(postobj['content'])
+            code_file.write("\n")
+            if comments:
+                for cm in comments:
+                    code_file.write("\n")
+                    code_file.write("* "*40)
+                    code_file.write("\n\n")
+                    code_file.write(cm['content'])
+                    code_file.write("\n\n")
+                    code_file.write("%s %s\n"%(cm['author'],cm["created"]))
+            code_file.close()
+            # sublime.active_window().run_command('close')
+            # code_view = sublime.active_window().open_file(post_file_path)
+            # sublime.active_window().focus_view(code_view)
+            sublime.message_dialog("Refresh ok")
+        except Exception,e:
+            sublime.error_message("error:%s"%e)  
+
+########################################################################################
+####   Talkincode.org add code                  
+########################################################################################
 
 class AddTicCode(sublime_plugin.TextCommand):
     def __init__(self,view):
@@ -95,28 +241,28 @@ class AddTicCode(sublime_plugin.TextCommand):
             titlegrp = re.search("@description:(.*)\n",content)
             tagsgrp = re.search("@tags:(.*)\n",content)
             idgrp = re.search("@id:(.*)\n",content)
-            if not titlegrp:
-                sublime.error_message(r"your source code must contains @description:{some text of title} ")
-            else:
-                if not sublime.ok_cancel_dialog("post code to talkincode.org,continue?"):
-                    return
-                title = titlegrp.group(1)
-                tags = None
-                if tagsgrp:
-                    tags=tagsgrp.group(1)
 
+            title = titlegrp and titlegrp.group(1)
+            tags = tagsgrp and tagsgrp.group(1)
+            pid = idgrp and idgrp.group(1)
+            if not title:
+                sublime.error_message(r"source code must contains @description:{some text of title} ")
+            else:
+                if not sublime.ok_cancel_dialog("submit code to talkincode.org,continue?"):
+                    return
                 filename = view.file_name()
                 fext = os.path.splitext(filename)[1]
                 if len(fext) >1:
                     fext = fext[1:]
-
-                params = dict(pid=(idgrp and idgrp.group(1) or 0),
+                lang = api.get_lang(fext)
+                tags = "%s,%s"%(lang,tags)
+                params = dict(pid=pid,
                     title=title,
                     author=settings.get("author"),
                     email=settings.get("email"),
                     tags=tags,
                     content=content,
-                    lang=api.get_lang(fext),
+                    lang=lang,
                     filename=os.path.basename(view.file_name()),
                     authkey=settings.get("authkey"))
                 
@@ -126,25 +272,108 @@ class AddTicCode(sublime_plugin.TextCommand):
                 else:
                     sublime.message_dialog("success")
         except Exception,e:
-            sublime.error_message("add code error %s "%e)
+            sublime.error_message("submit code error %s "%e)
+
+
+########################################################################################
+####   Talkincode.org add topic                  
+########################################################################################        
+
+class AddTicPostForm(sublime_plugin.TextCommand):
+    def __init__(self,view):
+        self.view = view           
+
+    def run(self, edit):
+        groupset = []
+        def cbk(idx):
+            if idx == -1:return
+            gname = groupset[idx]
+            grpid = gname[:gname.index("--")]
+            rview = sublime.active_window().new_file()
+            sublime.active_window().focus_view(rview)
+            rview.insert(edit,0,"Talkincode.org new Topic:\n@groupid:%s\n@title:\n@tags:\n@content:"%grpid)
+        try:
+            grps = api.get_groups()
+            for grp in grps:
+                groupset.append("%s--%s"%(grp["id"],grp["name"]))
+        except:
+            groupset = ["0--general group"]
+        sublime.active_window().show_quick_panel(groupset,cbk)
+
+    def is_visible(self):
+        return self.view.find("Talkincode.org new Topic:",0,sublime.IGNORECASE) is None         
 
 class AddTicPost(sublime_plugin.TextCommand):
     def __init__(self,view):
         self.view = view
-
+    def is_visible(self):
+        return self.view.find("Talkincode.org new Topic:",0,sublime.IGNORECASE) is not None           
     def run(self, edit):
-        pass      
+        try:
+            view = self.view
+            region = sublime.Region(0L, view.size())
+            content_src = view.substr(region)
+            titlegrp = re.search("@title:(.*)\n",content_src)
+            groupgrp = re.search("@groupid:(.*)\n",content_src)
+            tagsgrp = re.search("@tags:(.*)\n",content_src)
+            contentgrp = re.search("@content:(.*)",content_src)
+            
+            title = titlegrp and titlegrp.group(1)
+            content = contentgrp and contentgrp.group(1)
+            tags = tagsgrp and tagsgrp.group(1)
+            groupid = groupgrp and groupgrp.group(1) or 0
 
-class AddTicCodeComment(sublime_plugin.TextCommand):
-    def __init__(self,view):
-        self.view = view
+            if not title or not content:
+                sublime.error_message("title,content can not be empty")
+            else:
+                if not sublime.ok_cancel_dialog("submit topic to talkincode.org,continue?"):
+                    return
+                
+                params = dict(
+                    gid=groupid,
+                    title=title,
+                    tags=tags,
+                    content=content,
+                    authkey=settings.get("authkey"))
+                
+                result = api.add_post(params)
+                if result and result.has_key("error"):
+                    raise Exception(result["error"])
+                else:
+                    sublime.message_dialog("success")
+        except Exception,e:
+            sublime.error_message("submit topic error %s "%e)
 
-    def run(self, edit):
-        pass     
 
 class AddTicPostComment(sublime_plugin.TextCommand):
     def __init__(self,view):
         self.view = view
+    def is_visible(self):
+        return self.view.find(postid_flag,0) is not None 
 
-    def run(self, edit):
-        pass         
+    def run(self, edit):    
+        view = self.view
+        def on_input(cmtxt):
+            region = sublime.Region(0L, view.size())
+            content = view.substr(region)
+            postid_grp = re.search("%s(.*)\n"%postid_flag,content)
+            postid = postid_grp and postid_grp.group(1)  
+            try:
+                params = dict(postid=postid,content=cmtxt,authkey=settings.get("authkey"))
+                result = api.add_post_comment(params)
+                if result and result.has_key("error"):
+                    raise Exception(result["error"])
+                else:
+                    view.run_command("refresh_tic_post")
+                    #sublime.message_dialog("comment success")                
+            except Exception, e:
+                 sublime.error_message(str(e))
+
+        sublime.active_window().show_input_panel("Comment content::","",on_input,None,None)
+
+
+
+class BrowserTicSite(sublime_plugin.WindowCommand):
+    def run(self):
+        api.open_homepage()
+
--- api.py
+++ api.py
@@ -1,23 +1,31 @@
 import urllib,urllib2
-import logging
-import traceback
 import json
-import sublime
-
-logging.basicConfig(format='%(asctime)s %(message)s')
-logger = logging.getLogger()
 
 api_base_url = "http://127.0.0.1/api/"
 langset = {"java":"java","c":"c","cpp":"c++",
            "pas":"pascal","rb":"ruby","php":"php",
            "pl":"perl","js":"javascript","sql":"sql",
-           "lsp":"lisp","py":"python"}
+           "lsp":"lisp","py":"python",'css':'css','html':'html'}
+langextset = {'c': 'c', 'java': 'java', 'lisp': 'lsp', 
+              'javascript': 'js', 'c++': 'cpp', 'perl': 'pl',
+               'python': 'py', 'pascal': 'pas', 'sql': 'sql',
+                'php': 'php', 'ruby': 'rb','css':'css','html':'html'}
+
 
 def get_lang(ext):
     return  langset.get(ext,ext)
 
+def get_lang_ext(lang):
+    return langextset.get(lang,lang)
+
+
+def open_homepage():
+    import webbrowser
+    webbrowser.open("http://www.talkincode.org")
+        
+
+
 def do_request(params,url,method="get"):
-    logger.info( "post request, please wait......")
     try:
         data = urllib.urlencode(params) 
         if method == "post":
@@ -27,11 +35,14 @@ def do_request(params,url,method="get"):
             request = urllib2.Request("%s?%s"%(url,data))
             response = urllib2.urlopen(request)  
         else:
-            raise Exception("error method")
+            raise Exception("Method not allowed")
         return json.loads(response.read())
     except:
-        logger.error(" do_post error %s"%traceback.format_exc())
         raise
+
+def get_groups():
+    url = api_base_url + "groups"
+    return do_request({},url,"get")
 
 def register(username,password,email):
     params = dict(username=username,password=password,email=email)
@@ -42,6 +53,31 @@ def add_code(params):
     url = api_base_url + "code/add"
     return do_request(params,url,"post")
 
+def get_code(uid):
+    url = api_base_url + "code/get/%s"%uid
+    return do_request({},url,"get")
+
+def add_post(params):
+    url = api_base_url + "post/add"
+    return do_request(params,url,"post")
+
+def add_post_comment(params):
+    url = api_base_url + "comment/add"
+    return do_request(params,url,"post")    
+
+def get_post(uid):
+    url = api_base_url + "post/get/%s"%uid
+    return do_request({},url,"get")    
+
+def list_codes(q,limit=100):
+    params = dict(q=q,limit=limit)
+    url = api_base_url + "code/index"
+    return do_request(params,url,"get")
+
+def list_posts(q,limit=100):
+    params = dict(q=q,limit=limit)
+    url = api_base_url + "post/index"
+    return do_request(params,url,"get")
 
 if __name__ == "__main__":
-    print do_request({"q":"python"},"http://www.google.com.hk","get")
+    print get_lang_ext("python")
